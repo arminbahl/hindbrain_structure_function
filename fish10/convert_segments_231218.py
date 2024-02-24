aebf3b244f381e80be932f97450d2632f418856a
Vohra <bzfvohra@zib.zib.de>
2024-02-25 00:25:54 +0100

    included agglomerated query, file formats, layer id

--- fish10/convert_segments_231218.py
+++ fish10/convert_segments_231218.py
@@ -9,8 +9,7 @@ import db_dtypes  # type: ignore
 import util as ut 
 
 # setting up credentials to access google database
-# Please set path to mariela_fish_credentials.json
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = ''
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:\\Users\\bzfvohra\\Documents\\sumit\\laptop_data\\EM\\Em_bigdata_query\\mariela_fish_credentials.json'
 
 
 def segment_query(base_seg_str):
@@ -37,6 +36,30 @@ def segment_query(base_seg_str):
     df = bigquery_client.query(QUERY).to_dataframe()
     return df['seg_id_new'].tolist()
 
+def agglomerated_segments_query(base_seg_str):
+    """
+    fetching new segment ids from google cloud using old segment ids
+
+    arguments:
+        base_seg_str: str
+            comma separated segment ids
+    returns:
+        segments_list: list
+            neuron new segment ids
+    """
+    bigquery_client = bigquery.Client()
+    QUERY = """
+    SELECT
+        objects.id_a  as seg_id_base,
+        objects.id_b  as seg_id_agglomerated,
+    FROM
+        `engert-goog-connectomics.fish1_231218.240107b_ph12_spl_itr` as objects
+    WHERE objects.id_a in {}
+    """.format('('+base_seg_str+')')
+
+    df = bigquery_client.query(QUERY).to_dataframe()
+
+    return df
 
 def convert_mutiple_neurons(input_folder,skeleton_file, output_folder):
     """
@@ -58,14 +81,24 @@ def convert_mutiple_neurons(input_folder,skeleton_file, output_folder):
         convert_single_neuron(neuron_file, skeleton_file, output_folder)
         
 
-def convert_single_neuron(neuron_file, skeleton_file, output_folder):
+def convert_segments(neuron_file):
+    f=open(neuron_file, 'r')
+    neuron_json = json.load(f)
+    layers=neuron_json['layers']
+    segmentation_layer=layers[5]
+    base_segments=segmentation_layer['segments']  
+    base_seg_str = ','.join(str(x) for x in base_segments)
+    neuron_segments_list=segment_query(str(base_seg_str))
+    return neuron_segments_list
+
+def convert_single_neuron(neuron_file, layerId, skeleton_file, output_folder):
     sf=open(skeleton_file, 'r')
     skeleton_json = json.load(sf)
     base_name=os.path.basename(neuron_file)
     f=open(neuron_file, 'r')
     neuron_json = json.load(f)
     layers=neuron_json['layers']
-    segmentation_layer=layers[5]
+    segmentation_layer=layers[layerId]
     base_segments=segmentation_layer['segments']  
     base_seg_str = ','.join(str(x) for x in base_segments)
     neuron_segments_list=segment_query(str(base_seg_str))
--- fish10/parse_neurons_fish10.py
+++ fish10/parse_neurons_fish10.py
@@ -15,15 +15,16 @@ import convert_segments_231218 as convert_segments
 class EMSegmentsHelpers():
     def __init__(self, manual_opts_dict=dict()):
         self.set_opts_dict(manual_opts_dict)
-
+   
     def set_opts_dict(self, manual_opts_dict):
-        default_opts_dict = {
-            'cloudvolume_url':'precomputed://gs://fish1-public/seg_231218',
+         #'cloudvolume_url':'precomputed://gs://fish1-public/seg_231218',
+        default_opts_dict = {          
+            'cloudvolume_url':'precomputed://gs://fish1-public/seg_231218_agg240107b_ph12_spl',
             'old_segments': 'old_segments',
             'new_segments': 'new_segments',
             'output_data': 'output_data',
             'prefix_string':'https://neuroglancer-demo.appspot.com/#!',
-            'skeleton_file': 'skeleton_seg_231218.json',
+            'skeleton_file': 'skeleton_seg_231218_1.json',
         }
 
         for key in manual_opts_dict.keys():
@@ -65,13 +66,14 @@ class EMSegmentsHelpers():
         ut.create_directory(old_segments_folder)
         ut.create_directory(new_segments_folder)
         ut.create_directory(cells_data_folder)
-        df = pd.read_csv(neurons_file)
+        df = self.read_excel_file(neurons_file)
         for index, row in df.iterrows():
             cell_id=row['id']
             cell_name=row['name']
             cell_folder=os.path.join(cells_data_folder,cell_name)
             ut.create_directory(cell_folder)
             cell_units=row['units']
+            layerId=row['layer']
             cell_tracing_date=row['date_of_tracing']
             cell_tracers=row['tracer_name']
             cell_classifier=row['classifier']
@@ -82,8 +84,14 @@ class EMSegmentsHelpers():
             old_segments_data=json.loads(url)    
             old_json_file=os.path.join(old_segments_folder, cell_name+".json")
             ut.write_json(old_segments_data, old_json_file)
-            seg_ids=convert_segments.convert_single_neuron(old_json_file, skeleton_file, new_segments_folder)
-            self.download_neuron_segments(seg_ids,os.path.join(cell_folder, (cell_name+'.obj')) )
+            seg_ids=convert_segments.convert_single_neuron(old_json_file, layerId, skeleton_file, new_segments_folder)
+            base_seg_str = ','.join(str(x) for x in seg_ids)
+            segment_data_frame=convert_segments.agglomerated_segments_query(base_seg_str)
+            old_segments=segment_data_frame['seg_id_base'].tolist()
+            agglomerated_segments=segment_data_frame['seg_id_agglomerated'].tolist()
+            final_list = list(set(seg_ids) - set(old_segments))
+            final_list.extend(agglomerated_segments)
+            self.download_neuron_segments(final_list,os.path.join(cell_folder, (cell_name+'.obj')) )
             cell_soma=row['soma_position']
             cell_mece_regions=row['mece_regions']
             #cell_synapses=row['synapses']
@@ -117,6 +125,26 @@ class EMSegmentsHelpers():
             toml.dump(output_data, toml_file)
             print(cell_name)
 
+    def test_segment(self):
+        all_segments=[217030381, 200752462, 175738782, 122967887, 315052108, 208966913, 60212122, 138647536, 299111671, 239043596, 149756350, 53798951, 81605282, 392121992, 384255747, 192577630, 158346092, 160622366, 153347638, 300614500, 392871904, 234151035, 351914309, 249750438, 264200116, 42462198]
+        manifest_not_avail=[300614500, 299111671, 53798951, 192577630, 158346092, 264200116]
+        manifest_avail_segments=[217030381, 200752462, 175738782, 122967887, 315052108, 208966913, 60212122, 138647536, 239043596, 149756350, 81605282, 392121992, 384255747, 160622366, 153347638, 392871904, 234151035, 351914309, 249750438, 42462198]
+        #m = self.vol.mesh.get(all_segments, as_navis=True)
+        base_seg_str = ','.join(str(x) for x in all_segments)
+        segment_data_frame=convert_segments.agglomerated_segments_query(base_seg_str)
+        old_segments=segment_data_frame['seg_id_base'].tolist()
+        agglomerated_segments=segment_data_frame['seg_id_agglomerated'].tolist()
+        final_list = list(set(all_segments) - set(old_segments))
+        final_list.extend(agglomerated_segments)
+        m = self.vol.mesh.get(final_list, as_navis=True)
+        print(m)
+        print(agglomerated_segments)
+
+    def read_excel_file(self, file_name):
+        if(file_name.endswith('.csv')):
+            return pd.read_csv(neurons_file)
+        else:
+            return pd.read_excel(file_name)
 
 if __name__ == "__main__":
     print(len(sys.argv))
@@ -124,3 +152,6 @@ if __name__ == "__main__":
     output_folder=sys.argv[2]
     helper=EMSegmentsHelpers()
     helper.process_csv_file(neurons_file, output_folder)
+    #helper.test_segment()
+    #df=helper.read_excel_file(neurons_file)
+    #print(df)
