1b72393ee882a4a0688c4acaede5cd0986c6b379
Flo <florianfelixkampf@gmail.com>
2024-04-26 17:44:12 +0200

    FK | volume option | 20240426

--- visualization/FK_tools/load_brs.py
+++ visualization/FK_tools/load_brs.py
@@ -1,24 +1,34 @@
 import navis
 import os
 from pathlib import Path
-def load_brs(base_path,load_FK_regions=False):
+def load_brs(base_path,load_FK_regions=False,as_volume=True):
+    if as_volume:
+        load_type = 'volume'
+    else:
+        load_type = 'neuron'
     if load_FK_regions:
         meshes = []
 
         for file in os.listdir(base_path.joinpath("zbrain_regions").joinpath("FK_regions")):
             try:
                 meshes.append(navis.read_mesh(base_path.joinpath("zbrain_regions").joinpath("FK_regions").joinpath(file),
-                                units='um'))
+                                units='um',output=load_type))
             except:
                 pass
 
+
     else:
         meshes = []
-        try:
-            for file in os.listdir(base_path.joinpath("zbrain_regions")):
-                meshes.append(navis.read_mesh(base_path.joinpath("zbrain_regions").joinpath(file),
-                                units='um'))
-        except:
-            pass
+
+
+        for file in os.listdir(base_path.joinpath("zbrain_regions")):
+            if as_volume:
+                try:
+                    meshes.append(navis.read_mesh(base_path.joinpath("zbrain_regions").joinpath(file),
+                                    units='um',output=load_type))
+                except:
+                    pass
+
+
 
     return meshes
