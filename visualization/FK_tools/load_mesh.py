b444665c5e3b6d76a443f8c71848124ed25b87c3
Flo <florianfelixkampf@gmail.com>
2024-05-16 11:57:07 +0200

    FK | simplified how meshes/swc are loaded and added em | 20240516

--- visualization/FK_tools/load_mesh.py
+++ visualization/FK_tools/load_mesh.py
@@ -2,98 +2,75 @@ import numpy as np
 import navis
 import pandas as pd
 from pathlib import Path
-
+import math
 import warnings
+
+import numpy as np
+import navis
+import warnings
+from pathlib import Path
+
 warnings.filterwarnings("ignore")
-def load_mesh(cell,path,swc=False,use_smooth_pa=False):
-    if swc:
-        if use_smooth_pa:
-            if use_smooth_pa:
-                pa_path = path.joinpath('paGFP').joinpath(cell.cell_name)
 
-                if pa_path.joinpath(cell.cell_name + '_smoothed.swc').exists():
-                    cell['neurites_mesh'] = navis.read_swc(pa_path.joinpath(cell.cell_name + '_smoothed.swc'), units="um")
-                else:
-                    cell['neurites_mesh'] = np.nan
+def load_mesh(cell, path, swc=False, use_smooth_pa=False, load_both=False):
+    if cell['imaging_modality'] == 'clem':
+        cell_name_clem = f'clem_zfish1_{cell.type}_{cell.cell_name}'
+        clem_path = path / 'clem_zfish1' / 'all_cells' / cell_name_clem / 'mapped'
+    elif cell['imaging_modality'] == 'EM':
+        cell_name_em = f'em_fish1_{cell.cell_name}'
+        path_seed_cells_em = path / 'em_zfish1' / 'data_seed_cells' / 'output_data' / cell_name_em / 'mapped'
+        path_postsynaptic_em = path / 'em_zfish1' / 'data_cell_89189_postsynaptic_partners' / 'output_data' / cell_name_em / 'mapped'
+    elif cell['imaging_modality'] == 'photoactivation':
+        pa_path = path / 'paGFP' / str(cell.cell_name)
 
-        
+    if load_both:
+        swc = True
+
+    def load_file(file_path, file_type, is_swc=False):
+        if file_path.exists():
+
+                return navis.read_swc(file_path, units="um", read_meta=False) if is_swc else navis.read_mesh(file_path, units="um")
         else:
-            if cell['imaging_modality'] == 'clem':
-    
-                cell_name = 'clem_zfish1_' + cell.type + '_' + str(cell.cell_name)
-                clem_path = path.joinpath('clem_zfish1').joinpath('all_cells').joinpath(cell_name).joinpath('mapped')
-    
-    
-                if clem_path.joinpath(cell_name + '_mapped.swc').exists():
-                    cell['swc'] = navis.read_swc(clem_path.joinpath(cell_name + '_mapped.swc'), units="um")
-                else:
-                    print(f"No cell found at {clem_path.joinpath(cell_name + '_mapped.swc')}")
-                    cell['swc'] = np.nan
-                return cell
-    
-            if cell['imaging_modality'] == 'photoactivation':
-                pa_path = path.joinpath('paGFP').joinpath(cell.cell_name)
-    
-                if pa_path.joinpath(cell.cell_name + '.swc').exists():
-                    cell['swc'] = navis.read_swc(pa_path.joinpath(cell.cell_name + '.swc'), units="um")
-                else:
-                    cell['swc'] = np.nan
-                return cell
-        
-    else:
-        if cell['imaging_modality'] == 'clem':
-    
-            cell_name = 'clem_zfish1_' + cell.type + '_'+ str(cell.cell_name)
-            clem_path = path.joinpath('clem_zfish1').joinpath('all_cells').joinpath(cell_name).joinpath('mapped')
-    
-            
-            if clem_path.joinpath(cell_name + '_axon_mapped.obj').exists():
-                cell['axon_mesh'] = navis.read_mesh(clem_path.joinpath(cell_name  + '_axon_mapped.obj'),units="um")
-            else:
-                print(f"No axon found at {clem_path.joinpath(cell_name  + '_axon_mapped.obj')}")
-                cell['axon_mesh'] = np.nan
-    
-            if clem_path.joinpath(cell_name  + '_dendrite_mapped.obj').exists():
-                cell['dendrite_mesh'] = navis.read_mesh(clem_path.joinpath(cell_name  + '_dendrite_mapped.obj'),units="um")
-            else:
-                print(f"No dendrite found at {clem_path.joinpath(cell_name  + '_dendrite_mapped.obj')}")
-                cell['dendrite_mesh'] = np.nan
-    
-            if clem_path.joinpath(cell_name  + '_soma_mapped.obj').exists():
-                cell['soma_mesh'] = navis.read_mesh(clem_path.joinpath(cell_name + '_soma_mapped.obj'),units="um")
-            else:
-                print(f"No soma found at {clem_path.joinpath(cell_name  + '_soma_mapped.obj')}")
-                cell['soma_mesh'] = np.nan
-            return cell
-    
+            print(f"No {file_type} found at {file_path}")
+            return np.nan
+
+    if swc:
+        file_suffix = '_smoothed.swc' if use_smooth_pa else '.swc'
         if cell['imaging_modality'] == 'photoactivation':
-            if use_smooth_pa:
-                pa_path = path.joinpath('paGFP').joinpath(cell.cell_name)
+            file_path = pa_path / f'{cell.cell_name}{file_suffix}'
+            cell['swc'] = load_file(file_path, 'SWC', is_swc=True)
+        elif cell['imaging_modality'] == 'clem':
+            file_path = clem_path / f'{cell_name_clem}_mapped.swc'
+            cell['swc'] = load_file(file_path, 'SWC', is_swc=True)
+        elif cell['imaging_modality'] == 'EM':
+                if path_seed_cells_em.exists():
+                    file_path = path_seed_cells_em / f'{cell_name_em}_mapped.swc'
 
-                if pa_path.joinpath(cell.cell_name + '.obj').exists():
-                    cell['neurites_mesh'] = navis.read_mesh(pa_path.joinpath(cell.cell_name + '_smoothed.obj'), units="um")
-                else:
-                    cell['neurites_mesh'] = np.nan
-
-                if pa_path.joinpath(cell.cell_name + '_soma.obj').exists():
-                    cell['soma_mesh'] = navis.read_mesh(pa_path.joinpath(cell.cell_name + '_soma.obj'), units="um")
-                else:
-                    cell['soma_mesh'] = np.nan
-                return cell
-
-            else:
-                pa_path = path.joinpath('paGFP').joinpath(cell.cell_name)
+                    cell['swc'] = load_file(file_path, 'SWC', is_swc=True)
+                elif path_postsynaptic_em.exists():
+                    file_path = path_postsynaptic_em / f'{cell_name_em}_mapped.swc'
+                    cell['swc'] = load_file(file_path, 'SWC', is_swc=True)
 
 
-                if pa_path.joinpath(cell.cell_name + '.obj').exists():
-                    cell['neurites_mesh'] = navis.read_mesh(pa_path.joinpath(cell.cell_name + '.obj'),units="um")
-                else:
-                    cell['neurites_mesh'] = np.nan
+    if not swc or load_both:
+        if cell['imaging_modality'] == 'clem':
+            cell['axon_mesh'] = load_file(clem_path / f'{cell_name_clem}_axon_mapped.obj', 'axon')
+            cell['dendrite_mesh'] = load_file(clem_path / f'{cell_name_clem}_dendrite_mapped.obj', 'dendrite')
+            cell['soma_mesh'] = load_file(clem_path / f'{cell_name_clem}_soma_mapped.obj', 'soma')
+        elif cell['imaging_modality'] == 'EM':
+            if path_seed_cells_em.exists():
+                file_path = path_seed_cells_em
+            elif path_postsynaptic_em.exists():
+                file_path = path_postsynaptic_em
 
 
-                if pa_path.joinpath(cell.cell_name + '_soma.obj').exists():
-                    cell['soma_mesh'] = navis.read_mesh(pa_path.joinpath(cell.cell_name + '_soma.obj'),units="um")
-                else:
-                    cell['soma_mesh'] = np.nan
-                return cell
 
+            cell['axon_mesh'] = load_file(file_path / f'{cell_name_em}_axon_mapped.obj', 'axon')
+            cell['dendrite_mesh'] = load_file(file_path / f'{cell_name_em}_dendrite_mapped.obj', 'dendrite')
+            cell['soma_mesh'] = load_file(file_path / f'{cell_name_em}_soma_mapped.obj', 'soma')
+        elif cell['imaging_modality'] == 'photoactivation':
+            file_suffix = '_smoothed.obj' if use_smooth_pa else '.obj'
+            cell['neurites_mesh'] = load_file(pa_path / f'{cell.cell_name}{file_suffix}', 'neurites')
+            cell['soma_mesh'] = load_file(pa_path / f'{cell.cell_name}_soma.obj', 'soma')
+
+    return cell
