add2db033e888684c09d4fbb4a10e32a59131c52
Flo <florianfelixkampf@gmail.com>
2024-05-25 14:48:01 +0200

    FK | load more of gregors cells | 20240525

--- visualization/FK_tools/load_mesh.py
+++ visualization/FK_tools/load_mesh.py
@@ -18,8 +18,11 @@ def load_mesh(cell, path, swc=False, use_smooth_pa=False, load_both=False):
         clem_path = path / 'clem_zfish1' / 'all_cells' / cell_name_clem / 'mapped'
     elif cell['imaging_modality'] == 'EM':
         cell_name_em = f'em_fish1_{cell.cell_name}'
+
         path_seed_cells_em = path / 'em_zfish1' / 'data_seed_cells' / 'output_data' / cell_name_em / 'mapped'
-        path_postsynaptic_em = path / 'em_zfish1' / 'data_cell_89189_postsynaptic_partners' / 'output_data' / cell_name_em / 'mapped'
+        path_89189_postsynaptic_em = path / 'em_zfish1' / 'data_cell_89189_postsynaptic_partners' / 'output_data' / cell_name_em / 'mapped'
+        path_10_postsynaptic_em = path / 'em_zfish1' / 'cell_010_postsynaptic_partners' / 'output_data' / cell_name_em / 'mapped'
+        path_11_postsynaptic_em = path / 'em_zfish1' / 'cell_011_postsynaptic_partners' / 'output_data' / cell_name_em / 'mapped'
     elif cell['imaging_modality'] == 'photoactivation':
         pa_path = path / 'paGFP' / str(cell.cell_name)
 
@@ -47,8 +50,14 @@ def load_mesh(cell, path, swc=False, use_smooth_pa=False, load_both=False):
                     file_path = path_seed_cells_em / f'{cell_name_em}_mapped.swc'
 
                     cell['swc'] = load_file(file_path, 'SWC', is_swc=True)
-                elif path_postsynaptic_em.exists():
-                    file_path = path_postsynaptic_em / f'{cell_name_em}_mapped.swc'
+                elif path_89189_postsynaptic_em.exists():
+                    file_path = path_89189_postsynaptic_em / f'{cell_name_em}_mapped.swc'
+                    cell['swc'] = load_file(file_path, 'SWC', is_swc=True)
+                elif path_10_postsynaptic_em.exists():
+                    file_path = path_10_postsynaptic_em / f'{cell_name_em}_mapped.swc'
+                    cell['swc'] = load_file(file_path, 'SWC', is_swc=True)
+                elif path_11_postsynaptic_em.exists():
+                    file_path = path_11_postsynaptic_em / f'{cell_name_em}_mapped.swc'
                     cell['swc'] = load_file(file_path, 'SWC', is_swc=True)
 
 
@@ -57,11 +66,16 @@ def load_mesh(cell, path, swc=False, use_smooth_pa=False, load_both=False):
             cell['axon_mesh'] = load_file(clem_path / f'{cell_name_clem}_axon_mapped.obj', 'axon')
             cell['dendrite_mesh'] = load_file(clem_path / f'{cell_name_clem}_dendrite_mapped.obj', 'dendrite')
             cell['soma_mesh'] = load_file(clem_path / f'{cell_name_clem}_soma_mapped.obj', 'soma')
+            cell['all_mesh'] = load_file(clem_path / f'{cell_name_clem}_mapped.obj', 'soma')
         elif cell['imaging_modality'] == 'EM':
             if path_seed_cells_em.exists():
                 file_path = path_seed_cells_em
-            elif path_postsynaptic_em.exists():
-                file_path = path_postsynaptic_em
+            elif path_89189_postsynaptic_em.exists():
+                file_path = path_89189_postsynaptic_em
+            elif path_10_postsynaptic_em.exists():
+                file_path = path_10_postsynaptic_em
+            elif path_11_postsynaptic_em.exists():
+                file_path = path_11_postsynaptic_em
 
 
 
@@ -72,5 +86,6 @@ def load_mesh(cell, path, swc=False, use_smooth_pa=False, load_both=False):
             file_suffix = '_smoothed.obj' if use_smooth_pa else '.obj'
             cell['neurites_mesh'] = load_file(pa_path / f'{cell.cell_name}{file_suffix}', 'neurites')
             cell['soma_mesh'] = load_file(pa_path / f'{cell.cell_name}_soma.obj', 'soma')
+            cell['all_mesh'] = load_file(clem_path / f'{cell_name_clem}_combined.obj', 'Combined file')
 
     return cell
