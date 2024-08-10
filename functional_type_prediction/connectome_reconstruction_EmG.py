762b1f93f4870e9603f21d32b0840524335404c0
Flo <florianfelixkampf@gmail.com>
2024-08-10 12:37:42 +0200

    FK | predicting full connectomes jon with no functional cells | 20240810

--- functional_type_prediction/connectome_reconstruction_EmG.py
+++ functional_type_prediction/connectome_reconstruction_EmG.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
     path_to_data = get_base_path()
     brain_meshes = load_brs(path_to_data, 'raphe')
     width_brain = 495.56
-    path_to_figure_dir = path_to_data / 'make_figures_FK_output' / 'EmG_connectome_prediction'
+    path_to_figure_dir = path_to_data / 'make_figures_FK_output' / 'EmJ_connectome_enhancement'
     os.makedirs(path_to_figure_dir,exist_ok=True)
 
     #load cells
@@ -164,16 +164,23 @@ if __name__ == '__main__':
         meta_data_list = [x for x in t.split('\n') if x != ""]
 
         cells_em.loc[i,'prediction'] = extract_prediction(meta_data_list[12])
+        cells_em.loc[i, 'all_proba'] = meta_data_list[12]
+        temp_predictions = [x.strip('"') for x in meta_data_list[12].split(' ')[2:]]
+        cells_em.loc[i, 'proba_DT'] = eval(temp_predictions[0].split(':')[1])
+        cells_em.loc[i, 'proba_CI'] = eval(temp_predictions[1].split(':')[1])
+        cells_em.loc[i, 'proba_II'] = eval(temp_predictions[2].split(':')[1])
+        cells_em.loc[i, 'proba_MC'] = eval(temp_predictions[3].split(':')[1])
         cells_em.loc[i, 'prediction_reduced'] = extract_prediction(meta_data_list[13])
         cells_em.loc[i, 'nblast_test'] = eval(meta_data_list[14].split(' ')[2])
-        cells_em.loc[i, 'proba_test'] = eval(meta_data_list[15].split(' ')[2])
-        cells_em.loc[i, 'proba_test_reduced'] = eval(meta_data_list[16].split(' ')[2])
-        cells_em.loc[i, 'nov_OCSVM'] = eval(meta_data_list[17].split(' ')[2])
-        cells_em.loc[i, 'nov_OCSVM_reduced'] = eval(meta_data_list[18].split(' ')[2])
-        cells_em.loc[i, 'nov_IF'] = eval(meta_data_list[19].split(' ')[2])
-        cells_em.loc[i, 'nov_IF_reduced'] = eval(meta_data_list[20].split(' ')[2])
-        cells_em.loc[i, 'nov_LOF'] = eval(meta_data_list[21].split(' ')[2])
-        cells_em.loc[i, 'nov_LOF_reduced'] = eval(meta_data_list[22].split(' ')[2])
+        cells_em.loc[i, 'nblast_test_specific'] = eval(meta_data_list[15].split(' ')[2])
+        cells_em.loc[i, 'proba_test'] = eval(meta_data_list[16].split(' ')[2])
+        cells_em.loc[i, 'proba_test_reduced'] = eval(meta_data_list[17].split(' ')[2])
+        cells_em.loc[i, 'nov_OCSVM'] = eval(meta_data_list[18].split(' ')[2])
+        cells_em.loc[i, 'nov_OCSVM_reduced'] = eval(meta_data_list[19].split(' ')[2])
+        cells_em.loc[i, 'nov_IF'] = eval(meta_data_list[20].split(' ')[2])
+        cells_em.loc[i, 'nov_IF_reduced'] = eval(meta_data_list[21].split(' ')[2])
+        cells_em.loc[i, 'nov_LOF'] = eval(meta_data_list[22].split(' ')[2])
+        cells_em.loc[i, 'nov_LOF_reduced'] = eval(meta_data_list[23].split(' ')[2])
 
     #connectomeS
     def color_weakener(hex_color,factor=0.75):
@@ -213,7 +220,7 @@ if __name__ == '__main__':
 
 
 
-    for presynaptic_cell in [x for x in cells_em.presynaptic.unique() if x != 'seed_cell']:
+    for presynaptic_cell in tqdm(x for x in cells_em.presynaptic.unique() if x != 'seed_cell'):
         temp_connectome_df = cells_em.loc[(cells_em['presynaptic'] == presynaptic_cell) | (cells_em['cell_name'] == presynaptic_cell),:]
         color_list = []
         swc_list = []
@@ -237,7 +244,7 @@ if __name__ == '__main__':
                 swc_list[-1].name = "_".join(swc_list[-1].name.split('_')[:3])
 
 
-                soma_mesh_list.append(cell['soma_mesh'])
+                soma_mesh_list.append(navis.simplify_mesh(cell['soma_mesh'],1000))
                 soma_mesh_list[-1].name = swc_list[-1].name
 
                 dendrite_mesh_list.append(cell['dendrite_mesh'])
@@ -289,12 +296,18 @@ if __name__ == '__main__':
         ylim = [-30, 300]
 
         navis.plot2d(brain_meshes, color=color_meshes,
-                     alpha=0.2, linewidth=0.5, method='2d', view=view, group_neurons=True,
-                     rasterize=False, ax=ax)
+                     alpha=0.2, linewidth=0.5, method='2d', view=view, group_neurons=False,
+                     rasterize=True, ax=ax,
+                         scalebar="20 um")
+
+
 
         navis.plot2d(swc_list, color=color_list, alpha=1, linewidth=0.5,
-                     method='2d', view=view, group_neurons=True, rasterize=False, ax=ax,
-                     scalebar="20 um")
+                         method='2d', view=view, group_neurons=True, rasterize=False, ax=ax)
+        navis.plot2d(soma_mesh_list, color=color_list, alpha=1, linewidth=0.5,
+                         method='2d', view=view, group_neurons=True, rasterize=True, ax=ax)
+
+
 
         # navis.plot2d(axon_mesh_list, color=color_list, alpha=1, linewidth=0.5,
         #              method='2d', view=view, group_neurons=True, rasterize=False, ax=ax,
@@ -302,9 +315,13 @@ if __name__ == '__main__':
         # navis.plot2d(dendrite_mesh_list, color=color_list, alpha=1, linewidth=0.5,
         #              method='2d', view=view, group_neurons=True, rasterize=False, ax=ax,
         #              scalebar="20 um")
-        navis.plot2d(soma_mesh_list, color=color_list, alpha=1, linewidth=0.5,
-                     method='2d', view=view, group_neurons=True, rasterize=False, ax=ax,
-                     scalebar="20 um")
+        # navis.plot2d(swc_list, color=color_list, alpha=1, linewidth=0.5,
+        #              method='2d', view=view, group_neurons=True, rasterize=False, ax=ax,
+        #              scalebar="20 um")
+        #
+        # navis.plot2d(soma_mesh_list, color=color_list, alpha=1, linewidth=0.5,
+        #              method='2d', view=view, group_neurons=True, rasterize=False, ax=ax,
+        #              scalebar="20 um")
 
         ax.set_aspect('equal')
         ax.axvline(250, color=(0.85, 0.85, 0.85, 0.2), linestyle='--', alpha=0.5, zorder=0)
@@ -312,7 +329,7 @@ if __name__ == '__main__':
         # Set specific limits for the Y-axis based on the projection.
         ax.set_facecolor('white')
         ax.axis('off')
-        fig.savefig(path_to_figure_dir / f'{presynaptic_cell}_predicted_connectome_z_projection.pdf', dpi=1200)
+        fig.savefig(path_to_figure_dir / f'{presynaptic_cell}_predicted_connectome_z_projection.pdf', dpi=600)
 
 
         fig = plt.figure(figsize=(12, 12))
@@ -329,15 +346,21 @@ if __name__ == '__main__':
 
         navis.plot2d(brain_meshes, color=color_meshes,
                      alpha=0.2, linewidth=0.5, method='2d', view=view, group_neurons=True,
-                     rasterize=False, ax=ax)
+                     rasterize=True, ax=ax,scalebar="20 um")
 
         navis.plot2d(swc_list, color=color_list, alpha=1, linewidth=0.5,
-                     method='2d', view=view, group_neurons=True, rasterize=False, ax=ax,
-                     scalebar="20 um")
-
+                         method='2d', view=view, group_neurons=True, rasterize=False, ax=ax)
         navis.plot2d(soma_mesh_list, color=color_list, alpha=1, linewidth=0.5,
-                     method='2d', view=view, group_neurons=True, rasterize=False, ax=ax,
-                     scalebar="20 um")
+                         method='2d', view=view, group_neurons=True, rasterize=True, ax=ax)
+
+
+        # navis.plot2d(swc_list, color=color_list, alpha=1, linewidth=0.5,
+        #              method='2d', view=view, group_neurons=True, rasterize=False, ax=ax,
+        #              scalebar="20 um")
+        #
+        # navis.plot2d(soma_mesh_list, color=color_list, alpha=1, linewidth=0.5,
+        #              method='2d', view=view, group_neurons=True, rasterize=False, ax=ax,
+        #              scalebar="20 um")
 
         # navis.plot2d(axon_mesh_list, color=color_list, alpha=1, linewidth=0.5,
         #              method='2d', view=view, group_neurons=True, rasterize=False, ax=ax,
@@ -355,6 +378,6 @@ if __name__ == '__main__':
         # Set specific limits for the Y-axis based on the projection.
         ax.set_facecolor('white')
         ax.axis('off')
-        fig.savefig(path_to_figure_dir / f'{presynaptic_cell}_predicted_connectome_y_projection.pdf', dpi=1200)
+        fig.savefig(path_to_figure_dir / f'{presynaptic_cell}_predicted_connectome_y_projection.pdf', dpi=600)
 
     winsound.Beep(400, 150)
