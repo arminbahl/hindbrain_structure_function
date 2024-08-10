ef750f01f51f3bd5a935f7ea476bc07854de5168
Flo <florianfelixkampf@gmail.com>
2024-08-10 12:40:52 +0200

    FK | load cells swc/mesh better | 20240810

--- visualization/FK_tools/load_mesh.py
+++ visualization/FK_tools/load_mesh.py
@@ -17,10 +17,13 @@ def load_mesh(cell, path, swc=False, use_smooth_pa=False, load_both=False,load_r
         cell_name_clem = f'clem_zfish1_{cell.cell_name}'
         clem_path = path / 'clem_zfish1' / 'all_cells' / cell_name_clem / 'mapped'
         clem_path_to_predict = path / 'clem_zfish1' / 'to_predict' / cell_name_clem / 'mapped'
+        clem_path_neg_controls = path / 'clem_zfish1' / 'neg_controls' / cell_name_clem / 'mapped'
     elif cell['imaging_modality'] == 'clem' and load_repaired:
         cell_name_clem = f'clem_zfish1_{cell.cell_name}'
         clem_path = path / 'clem_zfish1' / 'all_cells_repaired'
         clem_path_to_predict = path / 'clem_zfish1' / 'to_predict' / cell_name_clem / 'mapped'
+        regular_clem_path = path / 'clem_zfish1' / 'all_cells' / cell_name_clem / 'mapped'
+        clem_path_neg_controls = path / 'clem_zfish1' / 'neg_controls' / cell_name_clem / 'mapped'
 
 
 
@@ -32,6 +35,12 @@ def load_mesh(cell, path, swc=False, use_smooth_pa=False, load_both=False,load_r
         path_10_postsynaptic_em = path / 'em_zfish1' / 'cell_010_postsynaptic_partners' / 'output_data' / cell_name_em / 'mapped'
         path_11_postsynaptic_em = path / 'em_zfish1' / 'cell_011_postsynaptic_partners' / 'output_data' / cell_name_em / 'mapped'
         path_19_postsynaptic_em = path / 'em_zfish1' / 'cell_019_postsynaptic_partners' / 'output_data' / cell_name_em / 'mapped'
+        cell_name_em_obj = f'em_fish1_{cell.cell_name}'
+        path_seed_cells_em_obj = path / 'em_zfish1' / 'data_seed_cells' / 'output_data' / cell_name_em_obj / 'mapped'
+        path_89189_postsynaptic_em_obj = path / 'em_zfish1' / 'data_cell_89189_postsynaptic_partners' / 'output_data' / cell_name_em_obj / 'mapped'
+        path_10_postsynaptic_em_obj = path / 'em_zfish1' / 'cell_010_postsynaptic_partners' / 'output_data' / cell_name_em_obj / 'mapped'
+        path_11_postsynaptic_em_obj = path / 'em_zfish1' / 'cell_011_postsynaptic_partners' / 'output_data' / cell_name_em_obj / 'mapped'
+        path_19_postsynaptic_em_obj = path / 'em_zfish1' / 'cell_019_postsynaptic_partners' / 'output_data' / cell_name_em_obj / 'mapped'
     elif cell['imaging_modality'] == 'EM' and load_repaired:
         cell_name_em = f'em_zfish1_{cell.cell_name}'
         path_seed_cells_em =            path / 'em_zfish1' / 'all_cells_repaired'
@@ -72,8 +81,10 @@ def load_mesh(cell, path, swc=False, use_smooth_pa=False, load_both=False,load_r
         elif cell['imaging_modality'] == 'clem'  and not load_repaired:
             if clem_path.exists():
                 file_path = clem_path / f'{cell_name_clem}_mapped.swc'
-            else:
+            elif clem_path_to_predict.exists():
                 file_path = clem_path_to_predict / f'{cell_name_clem}_mapped.swc'
+            else:
+                file_path = clem_path_neg_controls / f'{cell_name_clem}_mapped.swc'
 
             cell['swc'] = load_file(file_path, 'SWC', is_swc=True)
 
@@ -81,8 +92,11 @@ def load_mesh(cell, path, swc=False, use_smooth_pa=False, load_both=False,load_r
         elif cell['imaging_modality'] == 'clem'  and  load_repaired:
             if clem_path.exists():
                 file_path = clem_path / f'{cell_name_clem}_repaired.swc'
-            else:
+            elif clem_path_to_predict.exists():
                 file_path = clem_path_to_predict / f'{cell_name_clem}_mapped.swc'
+
+            else:
+                file_path = clem_path_neg_controls / f'{cell_name_clem}_mapped.swc'
             cell['swc'] = load_file(file_path, 'SWC', is_swc=True)
 
 
@@ -113,10 +127,16 @@ def load_mesh(cell, path, swc=False, use_smooth_pa=False, load_both=False,load_r
 
     if not swc or load_both:
         if cell['imaging_modality'] == 'clem':
-            if clem_path.exists():
+            if (clem_path / f'{cell_name_clem}').exists():
                 file_path = clem_path / f'{cell_name_clem}'
-            else:
+            elif (clem_path_to_predict / f'{cell_name_clem}').exists():
                 file_path = clem_path_to_predict / f'{cell_name_clem}'
+            elif clem_path_neg_controls.exists():
+                file_path = clem_path_neg_controls / f'{cell_name_clem}_mapped.swc'
+
+            else:
+                file_path = regular_clem_path /  f'{cell_name_clem}'
+
 
             for component,element_type in zip(['_axon_mapped','_dendrite_mapped','_soma_mapped'],['axon','dendrite','soma']):
                 try:
@@ -124,7 +144,7 @@ def load_mesh(cell, path, swc=False, use_smooth_pa=False, load_both=False,load_r
                 except:
                     pass
             try:
-                cell['all_mesh'] = load_file(Path(str(file_path) + "mapped.obj"), 'soma')
+                cell['all_mesh'] = load_file(Path(str(file_path) + "_mapped.obj"), 'complete')
             except:
                 pass
         elif cell['imaging_modality'] == 'EM':
