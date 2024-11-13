cc46dfd86ef588d57aadc3e5a1680d3585694b0c
fkaempf <florian_kaempf@gmx.de>
2024-11-13 11:07:48 +0000

    Refactor NBLAST matrix calculation logic
    Added optional inclusion of 'kunst_neurons' and corresponding function labels for detailed classification during NBLAST matrix calculation. Improved neuron resampling and matching logic, ensuring more accurate functional type predictions.

--- functional_type_prediction/NBLAST/nblast_matrix_navis.py
+++ functional_type_prediction/NBLAST/nblast_matrix_navis.py
@@ -30,44 +30,73 @@ import navis
 from navis.nbl.smat import Lookup2d, LookupDistDotBuilder, Digitizer
 import h5py
 from tqdm import tqdm
+import pickle
 
-
-def load_zebrafish_nblast_matrix(return_smat_obj=False,prune=True,modalities=["pa"],path_to_data = None):
+def calculate_zebrafish_nblast_matrix(df,with_kunst=True,return_smat_obj=False,prune=True,modalities=["pa"],path_to_data = None):
     if path_to_data is  None:
         path_to_data = get_base_path()
-    all_cells = load_cells_predictor_pipeline(path_to_data=path_to_data, modalities=modalities, use_smooth=True)
-    if prune:
-        all_cells.loc[:, 'swc'] = [navis.prune_twigs(x, 5, recursive=True) for x in all_cells['swc']]
-        all_cells.loc[:, 'swc'] = [navis.prune_twigs(x, 20, recursive=True) for x in all_cells['swc']]
     kunst_neurons = []
+    if with_kunst:
+        with h5py.File(path_to_data / 'zbrain_regions' / 'all_mpin_single_cells.hdf5') as f:
+            for key1 in f.keys():
+                print(key1)
+                for key2 in tqdm(f[key1].keys(),total=len(f[key1].keys())):
+                    temp_df = pd.DataFrame(f[key1][key2])
+                    temp_df.columns = ['node_id', 'label', 'x', 'y', 'z', 'radius', 'parent_id']
+                    temp_df.loc[:,"x"] = temp_df.loc[:,"x"]*0.798
+                    temp_df.loc[:, "y"] = temp_df.loc[:, "y"] *0.798
+                    temp_df.loc[:, "z"] = temp_df.loc[:, "z"] *2
+                    kunst_neurons.append(navis.TreeNeuron(temp_df,units='1 micrometer'))
+    if with_kunst:
+        all_neurons = np.concatenate([list(df.loc[:, 'swc'].values), kunst_neurons])
+    else:
+        all_neurons = np.array(list(df.loc[:, 'swc'].values))
+    all_neurons = [navis.resample_skeleton(n, '1 micrometer') for n in all_neurons]
+    all_neurons_name = [x.name for x in all_neurons]
+    function_label = []
+    for cell_name in all_neurons_name:
+        if cell_name is None:
+            function_label.append('na')
+        else:
+            if 'clem' in cell_name:
+                temp = cell_name.split('_repaired')[0].split('zfish1_')[1]
+            elif 'em' in cell_name:
+                temp = cell_name.split('_repaired')[0].split('zfish1_')[1]
+            else:
+                temp = cell_name.split('_repaired')[0]
 
-    with h5py.File(r"Y:\Zebrafish atlases\z_brain_atlas\single_cells\all_mpin_single_cells.hdf5") as f:
-        for key1 in f.keys():
-            print(key1)
-            for key2 in tqdm(f[key1].keys(),total=len(f[key1].keys())):
-                temp_df = pd.DataFrame(f[key1][key2])
-                temp_df.columns = ['node_id', 'label', 'x', 'y', 'z', 'radius', 'parent_id']
-                temp_df.loc[:,"x"] = temp_df.loc[:,"x"]*0.798
-                temp_df.loc[:, "y"] = temp_df.loc[:, "y"] *0.798
-                temp_df.loc[:, "z"] = temp_df.loc[:, "z"] *2
-                kunst_neurons.append(navis.TreeNeuron(temp_df))
+            temp_function = df.loc[df['cell_name']==temp,'function'].iloc[0]
+            temp_morphology = df.loc[df['cell_name'] == temp, 'morphology'].iloc[0]
+            if temp_function  in ['neg_control', 'to_predict']:
+                temp_function = 'na'
+            if temp_function == 'integrator':
+                temp_function = temp_morphology + '_' + temp_function
 
-    original = list(all_cells.loc[all_cells['function'] == 'dynamic_threshold', 'swc'])
-    all = list(all_cells.loc[:, 'swc'])
-    all = kunst_neurons
-
-    dotprops = [navis.make_dotprops(n, k=5, resample=False) for n in original + all]
-    matching_pairs = [[idx, idx + len(original)] for idx in range(len(original))]
+            function_label.append(temp_function)
 
 
-    builder = LookupDistDotBuilder(dotprops, matching_pairs, use_alpha=True, seed=2021).with_bin_counts([10, 10])
+
+
+    matches = []
+    for i1 in np.arange(len(function_label))[np.array(function_label)!='na']:
+        for i2 in np.arange(len(function_label))[np.array(function_label)!='na']:
+            if function_label[i1] == function_label[i2] and i1 != i2:
+                matches.append([i1,i2])
+
+    dotprops = [navis.make_dotprops(n, k=5, resample=False) for n in all_neurons]
+    builder = LookupDistDotBuilder(dotprops, matches, use_alpha=True, seed=2021).with_bin_counts([10, 10])
     smat = builder.build()
     as_table = smat.to_dataframe()
-    as_table
+
+
+
     if return_smat_obj:
         return smat
+
     else:
         return as_table
+
+
 
 if __name__ == "__main__":
     def symmetric_log_transform(x, linthresh=1):
