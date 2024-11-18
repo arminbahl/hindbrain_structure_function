df69f19cbe5906b7a60ca5cf391440309071a749
fkaempf <florian_kaempf@gmx.de>
2024-11-18 17:50:11 +0000

    Update suffix generation logic and output filename structure
    Refactored suffix generation to correct condition checks and adjusted output filenames to include the count of contralateral dynamic threshold neurons. Added print statement to confirm completion of iterations.
    
    Signed-off-by: fkaempf <florian_kaempf@gmx.de>

--- functional_type_prediction/classifier_prediction/scripts2run_class_predictor/create_predictions_all_combinations.py
+++ functional_type_prediction/classifier_prediction/scripts2run_class_predictor/create_predictions_all_combinations.py
@@ -5,23 +5,24 @@ if __name__ == "__main__":
     predictor_og = class_predictor(Path(r'D:\hindbrain_structure_function\nextcloud'))
     predictor_og.load_cells_df(kmeans_classes=True, new_neurotransmitter=True, modalities=['pa', 'clem', 'em', 'clem_predict'], neg_control=True)
     for drop_growth_cone in [True, False]:
-        suffix = ''
-        if drop_growth_cone:
-            suffix = '_drop_growth_cone'
-
-        for drop_exits_volume in [True, False]:
-            if drop_exits_volume:
-                suffix += '_drop_exits_volume'
-            for drop_truncated in [True, False]:
-                if drop_truncated:
-                    suffix += '_drop_truncated'
+        for drop_exits_volume in [False, True]:
+            for drop_truncated in [False, True]:
                 for add_morphology_annotation in [True, False]:
-                    if add_morphology_annotation:
-                        suffix += '_add_morphology_annotation'
-                    for drop_neurotransmitter in [True, False]:
-                        if drop_neurotransmitter:
-                            suffix += '_drop_neurotransmitter'
+                    for drop_neurotransmitter in [False, False]:
                         for cv_method in ['ss','lpo']:
+                            suffix = ''
+                            if drop_growth_cone:
+                                suffix = '_no_gc'
+                            if drop_exits_volume:
+                                suffix += '_no_ev'
+                            if drop_truncated:
+                                suffix += 'no_t'
+                            if add_morphology_annotation:
+                                suffix += '_na'
+                            if drop_neurotransmitter:
+                                suffix += '_no_nt'
+
+
                             suffix += f'_{cv_method}'
 
                             predictor_copy = copy.deepcopy(predictor_og)
@@ -39,7 +40,15 @@ if __name__ == "__main__":
                             clf_fk = LinearDiscriminantAnalysis(solver='lsqr', shrinkage='auto')
 
                             predictor_copy.predict_cells(use_jon_priors=True,suffix=suffix)
-                            test.plot_neurons('EM', output_filename=f'EM_predicted_jon_priors{suffix}.html')
+                            no_c_DT = predictor_copy.prediction_predict_df.loc[(predictor_copy.prediction_predict_df.imaging_modality=='EM')&
+                                                                             (predictor_copy.prediction_predict_df.prediction=='dynamic_threshold')&
+                                                                             (predictor_copy.prediction_predict_df.morphology_clone=='contralateral')].shape[0]
+                            predictor_copy.plot_neurons('EM', output_filename=f'{no_c_DT}contraDT_EM_predicted_jon_priors{suffix}.html')
                             predictor_copy.predict_cells(use_jon_priors=False,suffix=suffix)
-                            predictor_copy.plot_neurons('EM', output_filename=f'EM_predicted{suffix}.html')
+                            no_c_DT = predictor_copy.prediction_predict_df.loc[(predictor_copy.prediction_predict_df.imaging_modality=='EM')&
+                                                                             (predictor_copy.prediction_predict_df.prediction=='dynamic_threshold')&
+                                                                             (predictor_copy.prediction_predict_df.morphology_clone=='contralateral')].shape[0]
+                            predictor_copy.plot_neurons('EM', output_filename=f'{no_c_DT}contraDT_EM_predicted{suffix}.html')
+
+                            print('it. finished')
 
