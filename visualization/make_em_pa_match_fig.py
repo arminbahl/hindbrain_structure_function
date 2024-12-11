1fca32e1bdae8ada234535afabf06d4cb26cbb41
fkaempf <florianfelixkampf@gmail.com>
2024-12-11 15:21:08 +0000

    Update cell selection and streamline repetitive code
    Revised the list of selected cells for analysis and replaced repetitive plotting loops with a single streamlined implementation. This simplifies the code, improves maintainability, and reduces redundancy while preserving functionality.
    
    Signed-off-by: fkaempf <florianfelixkampf@gmail.com>

--- visualization/make_em_pa_match_fig.py
+++ visualization/make_em_pa_match_fig.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          only_soma=True,
                          load_what='swc',
                          cell_type='')
-    ci.all_cells = ci.all_cells.loc[ci.all_cells['cell_name'].isin(['133478', '20230628.2'])]
+    ci.all_cells = ci.all_cells.loc[ci.all_cells['cell_name'].isin(['131334', '20230428.1'])]
     ci.plot_y_projection(show_brs=True,
                          which_brs="raphe",
                          force_new_cell_list=False,
@@ -66,52 +66,27 @@ if __name__ == "__main__":
                          plot_synapse_distribution=False)
     plt.show()
 
-    # MC 20230613.1
-    for em_cell in ['168586', '153284', '147321', '166323', '124806', '126984']:
-        mc1 = make_figures_FK(modalities=['pa', 'em'],
-                              keywords='all',
-                              use_smooth_pa=True,
-                              mirror=True,
-                              only_soma=True,
-                              load_what='swc',
-                              cell_type='')
-        mc1.all_cells = mc1.all_cells.loc[
-            mc1.all_cells['cell_name'].isin([em_cell, '20230613.1'])]  # 141963 - 20230508.1
-        mc1.plot_y_projection(show_brs=True,
-                              which_brs="raphe",
-                              force_new_cell_list=False,
-                              rasterize=False,
-                              black_neuron=False,
-                              standard_size=True,
-                              volume_outlines=True,
-                              background_gray=True,
-                              only_soma=False,
-                              midline=True,
-                              plot_synapse_distribution=False)
+    # MC 
 
-        plt.show()
+    mc = make_figures_FK(modalities=['pa', 'em'],
+                         keywords='all',
+                         use_smooth_pa=True,
+                         mirror=True,
+                         only_soma=True,
+                         load_what='swc',
+                         cell_type='')
+    mc.all_cells = mc.all_cells.loc[
+        mc.all_cells['cell_name'].isin(['66813', '20230527.1'])]  # 141963 - 20230508.1
+    mc.plot_y_projection(show_brs=True,
+                         which_brs="raphe",
+                         force_new_cell_list=False,
+                         rasterize=False,
+                         black_neuron=False,
+                         standard_size=True,
+                         volume_outlines=True,
+                         background_gray=True,
+                         only_soma=False,
+                         midline=True,
+                         plot_synapse_distribution=False)
 
-    # MC cell 20230527.1
-    for em_cell in ['66813', '125979']:
-        mc2 = make_figures_FK(modalities=['pa', 'em'],
-                              keywords='all',
-                              use_smooth_pa=True,
-                              mirror=True,
-                              only_soma=True,
-                              load_what='swc',
-                              cell_type='')
-        mc2.all_cells = mc2.all_cells.loc[
-            mc2.all_cells['cell_name'].isin([em_cell, '20230527.1'])]  # 141963 - 20230508.1
-        mc2.plot_y_projection(show_brs=True,
-                              which_brs="raphe",
-                              force_new_cell_list=False,
-                              rasterize=False,
-                              black_neuron=False,
-                              standard_size=True,
-                              volume_outlines=True,
-                              background_gray=True,
-                              only_soma=False,
-                              midline=True,
-                              plot_synapse_distribution=False)
-
-        plt.show()
+    plt.show()
