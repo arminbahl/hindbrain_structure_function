d03cb7ecac287f60fee9a1b41cf0bd7cf477a414
fkaempf <florianfelixkampf@gmail.com>
2024-12-13 15:08:58 +0000

    Add metadata path extraction in load_pa_table function
    Modified the load_pa_table function to include logic for extracting metadata file paths for each cell using glob and os. This enhancement ensures the metadata paths are added to the table, improving downstream data handling.
    
    Signed-off-by: fkaempf <florianfelixkampf@gmail.com>

--- visualization/FK_tools/load_pa_table.py
+++ visualization/FK_tools/load_pa_table.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pathlib import Path
-
+import os
+import glob
 def load_pa_table(path):
     pa_table = pd.read_csv(path)
     pa_table = pa_table[(~pa_table['cell_type_labels'].isna()) &
@@ -14,6 +15,13 @@ def load_pa_table(path):
         if column != 'cell_type_labels':
             pa_table[column] = pa_table[column].apply(lambda x: x.replace('"', '') if type(x) == str else x)
 
+    for i, cell in pa_table.iterrows():
+        try:
+            pa_table.loc[i, 'metadata_path'] = Path(
+                glob.glob(os.path.join(path.parent / cell.cell_name / "*_metadata.txt"))[0])
+        except:
+            pa_table.loc[i, 'metadata_path'] = Path(
+                glob.glob(os.path.join(path.parent / cell.cell_name / "*metadata.txt"))[0])
     return pa_table
 
 if __name__ == '__main__':
