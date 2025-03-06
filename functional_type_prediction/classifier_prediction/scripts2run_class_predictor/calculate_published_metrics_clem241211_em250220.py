5522f4b2f267581838aeb4a43d96cb5817ca0309
fkaempf <florianfelixkampf@gmail.com>
2025-03-06 10:37:54 +0000

    Update metrics calculation and classifier models in published metrics script

--- functional_type_prediction/classifier_prediction/scripts2run_class_predictor/calculate_published_metrics_clem241211_em250220.py
+++ functional_type_prediction/classifier_prediction/scripts2run_class_predictor/calculate_published_metrics_clem241211_em250220.py
@@ -10,18 +10,20 @@ if __name__ == "__main__":
     with_neurotransmitter.load_cells_df(kmeans_classes=True, new_neurotransmitter=True,
                                         modalities=['pa', 'clem241211', 'em', 'clem_predict241211'], neg_control=True,
                                         input_em=True)
-    with_neurotransmitter.calculate_metrics('FINAL_CLEM_CLEMPREDICT_EM_with_clem241211_withgregor241216')
+    with_neurotransmitter.calculate_metrics('FINAL_CLEM_CLEMPREDICT_EM_with_clem241211_withgregor250220')
 
     # with_neurotransmitter.calculate_published_metrics()
-    with_neurotransmitter.load_cells_features('FINAL_CLEM_CLEMPREDICT_EM_with_clem241211_withgregor241216',
+    with_neurotransmitter.load_cells_features('FINAL_CLEM_CLEMPREDICT_EM_with_clem241211_withgregor250220',
                                               with_neg_control=True,
                                               drop_neurotransmitter=False)
     with_neurotransmitter.calculate_published_metrics()
 
     n_estimators_rf = 100
-    clf_pv = KNeighborsClassifier()
-    clf_ps = KNeighborsClassifier()
-    clf_ff = RandomForestClassifier(n_estimators=n_estimators_rf)
+    #clf_pv = KNeighborsClassifier()
+    #clf_ps = KNeighborsClassifier()
+    clf_pv = LinearDiscriminantAnalysis(solver='lsqr', shrinkage='auto')
+    clf_ps = LinearDiscriminantAnalysis(solver='lsqr', shrinkage='auto')
+    clf_ff = LinearDiscriminantAnalysis(solver='lsqr', shrinkage='auto')
 
     with_neurotransmitter.confusion_matrices(clf_pv, method='lpo', plot_cm_order_jon=True, feature_type='pv')
     with_neurotransmitter.confusion_matrices(clf_ps, method='lpo', plot_cm_order_jon=True, feature_type='ps')
