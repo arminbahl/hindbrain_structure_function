49258deffed60319c29b3acaa3f9c9a03ef34e94
fkaempf <florianfelixkampf@gmail.com>
2025-03-06 10:38:11 +0000

    Add import statements for OS and sys modules in class predictor pipeline script

--- functional_type_prediction/classifier_prediction/scripts2run_class_predictor/pipeline_with_clem241211_withgregor250220.py
+++ functional_type_prediction/classifier_prediction/scripts2run_class_predictor/pipeline_with_clem241211_withgregor250220.py
@@ -1,5 +1,8 @@
 from holoviews.plotting.bokeh.styles import font_size
-
+import os
+import sys
+import os
+sys.path.extend(['/Users/fkampf/PycharmProjects'])
 from hindbrain_structure_function.functional_type_prediction.classifier_prediction.class_predictor import *
 from itertools import product
 from sklearn.metrics import f1_score
